446362aef2e511b240d9d5349e4e1894a1864199
jungwoonAn <red0808@naver.com>
2026-09-17 13:10:29 +0900

    260917:login/logout

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,14 +1,16 @@
 from datetime import datetime
 
-from flask import Blueprint, redirect, url_for, request, render_template
+from flask import Blueprint, redirect, url_for, request, render_template, g
 
 from pybo import db
 from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
+from pybo.views.auth_views import login_required
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=['POST'])
+@login_required
 def create(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
@@ -17,7 +19,7 @@ def create(question_id):
         # content = request.form['content']  # 폼태그에 입력된 값 받을 때
         content = form.content.data          # 폼모듈로 입력된 값 받을 때
 
-        answer = Answer(question= question, content=content, create_date=datetime.now())
+        answer = Answer(question= question, content=content, create_date=datetime.now(), user=g.user)
         db.session.add(answer)
         db.session.commit()
 
